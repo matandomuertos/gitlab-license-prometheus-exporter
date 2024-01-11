00379d38a60afd40228ff63e34290d6eaee0ea48
Nahuel Cassinari <matandomuertos@users.noreply.github.com>
2024-01-11 09:52:33 +0100

    Added new metrics -- in progress

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
-[![Build Status](https://jenkins.public.jamf.build/buildStatus/icon?job=Devops%2FBuild+Infrastructure%2FGitlab%2Fgitlab-exporter-github%2Fmaster)](https://jenkins.jamf.build/job/Devops/job/Build%20Infrastructure/job/Gitlab/job/gitlab-exporter-github/job/master/)
 
-# Gitlab license exporter
+
+# Gitlab license Prometheus exporter
 Exposes License expiration date and active users from the Gitlab API, to a Prometheus compatible endpoint.
+Based on (gitlab_license_exporter)[https://github.com/jamf/gitlab_license_exporter] which was archived by the company who own it.
 
-## Configuration
-This exporter is setup to take input from environment variables:
+## Parameters
+This exporter is setup to take input from these required environment variables:
+* `TOKEN`: Gitlab Admin API token (License is only exposed to administrators)
+* `URL`: Gitlab URL (ex. `https://gitlab.com`)
 
-### Required
-* `TOKEN`: Admin token
+## Metrics
+Metrics will be available on port 9090
 
-### Optional
-* `URL`: Gitlab url example: `https://gitlab.domain.com` (by default will use k8s service `gitlab-web`)
+| Name                      | Type  | Help                                                   |
+| ------------------------- | ----- | ------------------------------------------------------ |
+| gitlab_active_users       | gauge | Gitlab active users                                    |
+| gitlab_license_expires_at | gauge | Gitlab expiration day                                  |
+| gitlab_scrape_success     | gauge | Gitlab exporter scrape status when try to read the API |
+| gitlab_user_limit         | gauge | Users allowed by license                               |
 
 ## Build and run
 ### Manually
@@ -18,10 +25,10 @@ This exporter is setup to take input from environment variables:
 go get
 go build gitlabgoexporter.go
 export TOKEN=token123token
-export URL=https://gitlab.domain.com
+export URL=https://gitlab.com
 ./gitlabgoexporter.go
 ```
-Visit http://localhost:2222
+Visit http://localhost:9090
 
 
 ### Docker
@@ -34,7 +41,6 @@ Run:
 
 * Kubernetes Gitlab-Web service:
 	`docker image --env TOKEN=token123token <image-name>`
-
 
 ### Kubernetes
 ```
@@ -113,15 +119,4 @@ spec:
   - port: metrics
     path: /
     interval: 30s
-```
-
-## Metrics
-Metrics will be available on port 2222 by default
-
-## Collectors
-```
-# HELP gitlab_active_users Gitlab active users
-# HELP gitlab_license_expires_at Gitlab expiration day
-# HELP gitlab_scrape_success Gitlab go exporter scrape status when try to read the API
-# HELP gitlab_user_limit Users allowed by license
 ```
--- gitlabgoexporter.go
+++ gitlabgoexporter.go
@@ -1,148 +1,182 @@
 package main
 
 import (
-    "net/http"
+	"encoding/json"
+	"errors"
+	"io"
 	"log"
-	"io/ioutil"
-    "encoding/json"
-    "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
-    "time"
-    "errors"
-    "os"
-    "strconv"
-    "strings"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-    expiration = prometheus.NewGauge(prometheus.GaugeOpts{
-        Name: "gitlab_license_expires_at",
-        Help: "Gitlab expiration day",
-    })
+	activeUsers = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitlab_active_users",
+		Help: "Active users",
+	})
 
-    activeUsers = prometheus.NewGauge(prometheus.GaugeOpts{
-        Name: "gitlab_active_users",
-        Help: "Gitlab active users",
-    })
+	userLimit = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitlab_user_limit",
+		Help: "License user limit",
+	})
 
-    limitUsers = prometheus.NewGauge(prometheus.GaugeOpts{
-        Name: "gitlab_user_limit",
-        Help: "Users allowed by license",
-    })
+	expirationDate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitlab_license_expires_at",
+		Help: "Expiration date",
+	})
 
-    scrapeSuccess = prometheus.NewGauge(prometheus.GaugeOpts{
-        Name: "gitlab_scrape_success",
-        Help: "Gitlab go exporter scrape status when try to read the API",
-    })
+	maximumUserCount = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitlab_maximum_user_count",
+		Help: "Maximum number of users since license started",
+	})
+
+	expired = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitlab_license_expired",
+		Help: "License expiration status",
+	})
+
+	userOverage = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitlab_user_overage",
+		Help: "Difference between active users and licesed users",
+	})
+
+	scrapeSuccess = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gitlab_scrape_status",
+		Help: "Exporter scrape status",
+	})
 )
 
 type gitlab struct {
-    ActiveUsers float64 `json:"active_users"`
-    MaxUsers float64 `json:"user_limit"`
-    ExpiresDate string `json:"expires_at"`
-    ExpireSec float64
-}
-
-func main() {
-    url, err := seturl()
-    if err != nil{
-        log.Fatal(err)
-    }
-    recordMetrics(url)
-    http.Handle("/", promhttp.Handler())
-    log.Fatal(http.ListenAndServe(":2222", nil))
+	ActiveUsers      float64 `json:"active_users"`
+	UserLimit        float64 `json:"user_limit"`
+	ExpirationDate   string  `json:"expires_at"`
+	MaximumUserCount float64 `json:"maximum_user_count"`
+	Expired          bool    `json:"expired"`
+	UserOverage      float64 `json:"overage"`
+	ExpireSec        float64
+	IsExpired        float64
 }
 
 func init() {
-    prometheus.MustRegister(expiration)
-    prometheus.MustRegister(activeUsers)
-    prometheus.MustRegister(limitUsers)
-    prometheus.MustRegister(scrapeSuccess)
+	prometheus.MustRegister(activeUsers)
+	prometheus.MustRegister(userLimit)
+	prometheus.MustRegister(expirationDate)
+	prometheus.MustRegister(maximumUserCount)
+	prometheus.MustRegister(expired)
+	prometheus.MustRegister(userOverage)
+	prometheus.MustRegister(scrapeSuccess)
 }
 
-func seturl() (string, error) {
-    token := os.Getenv("TOKEN")
-    if token == "" {
-        return "", errors.New("Please set a token in env variable TOKEN")
-    }
+func validateEnvVars() (string, error) {
+	token, isSet := os.LookupEnv("GITLAB_TOKEN")
+	if !isSet {
+		return "", errors.New("Environment variable GITLAB_TOKEN is not set")
+	}
 
-    var url string
-    varURL := os.Getenv("URL")
-    if varURL == "" {
-        url = "http://gitlab-web:80"
-    } else {
-        url = strings.TrimRight(varURL, "/")
-    }
+	url, isSet := os.LookupEnv("GITLAB_URL")
+	if !isSet {
+		return "", errors.New("Environment variable GITLAB_URL is not set")
+	}
 
-    return string(url + "/api/v4/license?private_token=" + token), nil
+	return string(url + "/api/v4/license?private_token=" + token), nil
 }
 
 func recordMetrics(url string) {
-    go func() {
-        for {
-            body, err := getBody(url)
-            if err != nil {
-                log.Print(err)
-                scrapeSuccess.Set(0)
-            } else {
-                glab, err := parseGitlab(body)
-                if err != nil {
-                    log.Print(err)
-                    scrapeSuccess.Set(0)
-                } else{
-                    scrapeSuccess.Set(1)
-                    expiration.Set(glab.ExpireSec)
-                    activeUsers.Set(glab.ActiveUsers)
-                    limitUsers.Set(glab.MaxUsers)
-                    log.Print("Scrapped successfully")
-                }
-            }
-            time.Sleep(60 * time.Minute)
-        }
-    }()
+	go func() {
+		for {
+			body, err := getBody(url)
+			if err != nil {
+				log.Print(err)
+				scrapeSuccess.Set(0)
+			} else {
+				glab, err := parseGitlab(body)
+				if err != nil {
+					log.Print(err)
+					scrapeSuccess.Set(0)
+				} else {
+					scrapeSuccess.Set(1)
+
+					activeUsers.Set(glab.ActiveUsers)
+					userLimit.Set(glab.UserLimit)
+					expirationDate.Set(glab.ExpireSec)
+
+					log.Print("Scrapped successfully")
+				}
+			}
+			time.Sleep(60 * time.Minute)
+		}
+	}()
 }
 
-func getBody(url string) ([]byte, error){
+func getBody(url string) ([]byte, error) {
 
-    client := http.Client{
-        Timeout: 30 * time.Second,
-    }
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
 
-    resp, err := client.Get(url)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != 200 {
-        if resp.StatusCode == 401 {
-            return nil, errors.New("401 unauthorised")
-        } else if resp.StatusCode == 403 {
-            return nil, errors.New("403 The request requires higher privileges than provided by the access token.")
-        } else {
-            return nil, errors.New("Unexpected error. HTTP status code: " + strconv.Itoa(resp.StatusCode))
-        }
-    }
+	if resp.StatusCode != 200 {
+		if resp.StatusCode == 401 {
+			return nil, errors.New("401 unauthorised")
+		} else if resp.StatusCode == 403 {
+			return nil, errors.New("403 The request requires higher privileges than provided by the access token.")
+		} else {
+			return nil, errors.New("Unexpected error. HTTP status code: " + strconv.Itoa(resp.StatusCode))
+		}
+	}
 
-    responseData, err := ioutil.ReadAll(resp.Body)
-     if err != nil {
-        return nil, err
-    }
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-    return []byte(responseData), nil
+	return []byte(responseData), nil
 }
 
-func parseGitlab(textBytes []byte) (gitlab, error){
-    const layoutISO = "2006-01-02"
-    g := gitlab{}
+func parseGitlab(textBytes []byte) (gitlab, error) {
+	const layoutISO = "2006-01-02"
+	g := gitlab{}
 
-    json.Unmarshal(textBytes, &g)
+	json.Unmarshal(textBytes, &g)
 
-    t, err := time.Parse(layoutISO, g.ExpiresDate)
-    if err != nil {
-        return g, err
-    }
-    g.ExpireSec = float64(t.Unix())
+	t, err := time.Parse(layoutISO, g.ExpirationDate)
+	if err != nil {
+		return g, err
+	}
+	g.ExpireSec = float64(t.Unix())
 
-    return g, nil
+	g.IsExpired = isExpired(g.Expired)
+
+	return g, nil
+}
+
+func isExpired(expired bool) float64 {
+	switch expired {
+	case false:
+		return 0
+	case true:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func main() {
+	url, err := validateEnvVars()
+	if err != nil {
+		log.Fatal(err)
+	}
+	recordMetrics(url)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }
